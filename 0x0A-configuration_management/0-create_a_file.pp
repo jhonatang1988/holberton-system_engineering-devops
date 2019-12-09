9a9c874da9b2b6c3dddc92402574a0f4bd8ce8ea
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-12-09 20:38:24 +0000

    0 and 1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a files using puppet
 file{ '/tmp/holberton':
-  path => '/tmp/holberton',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love puppet',
 }
