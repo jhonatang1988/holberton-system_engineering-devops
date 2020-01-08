7503deb3732432cf1fd4c4c0df935833ce85dc9f
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-08 20:26:33 +0000

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,20 +1,20 @@
 # updates and install nginx, then creates a index.html
 exec { '/usr/bin/env apt-get -y update' : }
 -> package { 'nginx':
-   ensure => installed,
+  ensure => installed,
 }
 
 -> file_line { 'add header' :
-   ensure => present,
-   line   => "\tadd_header X-Served-By ${{hostname};",
-   path   => '/etc/nginx/sites-available/default',
-   after  => 'server_name _;',
+  ensure => present,
+  line   => "\tadd_header X-Served-By ${{hostname};",
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
 }
 
 -> file { '/var/www/html/index.html' :
-   content => 'Holberton School'
+  content => 'Holberton School'
 }
 
 -> service { 'nginx':
-   ensure => running,
+  ensure => running,
 }
