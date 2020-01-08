8dfb048e81c1a260cb45eef08afbb39529b7123c
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-08 20:20:26 +0000

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,5 +1,6 @@
 # updates and install nginx, then creates a index.html
 exec { 'installnginxwithcustomheader':
-  command  => 'sudo apt update ; sudo apt -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.html; sudo sed -i "s/sendfile on;/sendfile on;\n\tadd_header X-Served-By ${HOSTNAME;/" /etc/nginx/nginx.conf'; sudo service nginx restart,
-  provider => shell,
+  environment => ["ahostname=${hostname}"]
+  command     => 'sudo apt update ; sudo apt -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.html; sudo sed -i "s/sendfile on;/sendfile on;\n\tadd_header X-Served-By $ahostname;/" /etc/nginx/nginx.conf'; sudo service nginx restart',
+  provider    => shell,
 }
